ee2c6f69714dfa2c579c787689cca3c84c613990
Ionut Colceriu <contact@ghinda.net>
2014-04-15 22:16:15 +0300

    More testing on orders. Minor fixes on the order form texts.

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -12,7 +12,7 @@ app.controller('OrderCtrl', function($rootScope, $scope, $routeParams, $location
 		csc: '',
 		type: ''
 	};
-	
+
 	model.countries = [
 		{
 			name: 'USA',
@@ -32,11 +32,11 @@ app.controller('OrderCtrl', function($rootScope, $scope, $routeParams, $location
 	model.order.shippingDetailsType = 'same';
 	model.order.shippingDetailsCustom = false;
 	model.order.selectedOffer = 0;
-	
+
 	model.shipping.country = model.countries[0];
 
 	model.regions = data.model.regions;
-	
+
 	$scope.ImageUrl = function(url) {
 		return data.printchompUrl + url;
 	}
@@ -48,11 +48,11 @@ app.controller('OrderCtrl', function($rootScope, $scope, $routeParams, $location
 	$scope.$watch('model.order.country', function() {
 		model.order.region = model.regions[model.order.country.id][0];
 	});
-	
+
 	$scope.$watch('model.shipping.country', function() {
 		model.shipping.region = model.regions[model.shipping.country.id][0];
 	});
-	
+
 	$scope.$watch('model.order.shippingDetailsType', function() {
 		if(model.order.shippingDetailsType === 'custom') {
 			model.order.shippingDetailsCustom = true;
@@ -60,41 +60,41 @@ app.controller('OrderCtrl', function($rootScope, $scope, $routeParams, $location
 			model.order.shippingDetailsCustom = false;
 		}
 	});
-	
+
 	// only for testing
-// 	model.order.postcode = '35801';
-// 	model.order.address1 = 'H';
-// 	model.order.card.number = '4242424242424242';
-// 	model.order.card.exp = '12/2020';
-// 	model.order.card.csc = '123';
-	
+	model.order.postcode = '35801';
+	model.order.address1 = 'H';
+	model.order.card.number = '4242424242424242';
+	model.order.card.exp = '12/2020';
+	model.order.card.csc = '123';
+
 	$scope.SendOrder = function(event) {
-		
+
 		// make sure the form is valid
 		if(!$scope.orderForm.$valid) {
 			return false;
 		}
-		
+
 		var orderData = {};
-		
+
 		// generate base64 picture
 		$scope.generatePicture().then(function(canvas) {
-			
+
 			// send the canvas as a b64 jpeg
 			var jpegUrl = canvas.toDataURL('image/jpeg');
 			orderData.image = jpegUrl;
-			
+
 			// user details
 			orderData.user = {
 				email: model.order.email,
 				name: model.order.name
 			};
-			
+
 			// billing details
 			orderData.billing = {};
 			orderData.billing.name = model.order.name;
 			orderData.billing.phone = model.order.phone;
-			
+
 			// billing address
 			orderData.billing.address = {};
 			orderData.billing.address.city = model.order.city;
@@ -103,30 +103,30 @@ app.controller('OrderCtrl', function($rootScope, $scope, $routeParams, $location
 			orderData.billing.address.postal_code = model.order.postcode;
 			orderData.billing.address.street = model.order.address1;
 			orderData.billing.address.street2 = model.order.address2 || '';
-			
+
 			// offer details
 			orderData.billing.amount = angular.copy(model.offers[model.order.selectedOffer].amount);
-			
+
 			orderData.offer = {};
 			orderData.offer.id = model.offers[model.order.selectedOffer].id;
-			
+
 			// payment details
 			orderData.billing.credit_card = {};
 			orderData.billing.credit_card.number = model.order.card.number.replace(/ /g, '');
 			orderData.billing.credit_card.verification = model.order.card.csc;
-			
+
 			var exp = model.order.card.exp.replace(/ /g, '').split('/');
 			orderData.billing.credit_card.expiry = {};
 			orderData.billing.credit_card.expiry.month = exp[0];
 			orderData.billing.credit_card.expiry.year = exp[1];
-			
+
 			// shipping details
 			orderData.shipping = {};
 			if(model.order.shippingDetailsCustom) {
-				
+
 				orderData.shipping.name = model.shipping.name;
 				orderData.shipping.phone = model.shipping.phone;
-				
+
 				orderData.shipping.address = {};
 				orderData.shipping.address.city = model.shipping.city;
 				orderData.shipping.address.region = model.shipping.region.id;
@@ -134,51 +134,51 @@ app.controller('OrderCtrl', function($rootScope, $scope, $routeParams, $location
 				orderData.shipping.address.postal_code = model.shipping.postcode;
 				orderData.shipping.address.street = model.shipping.address1;
 				orderData.shipping.address.street2 = model.shipping.address2 || '';
-				
+
 			} else {
-				
+
 				orderData.shipping.name = orderData.billing.name;
 				orderData.shipping.phone = orderData.billing.phone;
 				orderData.shipping.address = orderData.billing.address;
-				
+
 			}
-		
+
 			model.orderLoading = true;
-		
+
 			data.SendOrder(orderData).then(function(response) {
-				
+
 				console.log('success', response);
-				
+
 				model.orderLoading = false;
-				
+
 				model.error = '';
 				model.orderSuccess = true;
-				
+
 				// hide the modal in 5s
 				$timeout(function() {
 					$('#modal-order').foundation('reveal', 'close');
 				}, 5000);
-				
+
 			}, function(err) {
-				
+
 				console.log('error', err);
-				
+
 				model.error = err.error;
 				model.orderLoading = false;
-				
+
 			});
-			
+
 			// scroll back to the bottom, because html2canvas takes us to the top*
 			window.scrollTo(0, document.body.scrollHeight - 950);
-			
+
 		});
-		
-		
+
+
 		// prevent form submission
 		// for some weird reason, angular doesn't prevent submission automatically
 		// only after I do a regular submit and a ? search param is set
 		event.preventDefault();
-		
+
 	};
 
 });
