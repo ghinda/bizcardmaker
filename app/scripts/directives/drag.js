3262585e1b4b0fb1603dc8907690d197d29ead81
Sebi Kovacs <sebi.kovacs@gmail.com>
2014-04-16 09:59:51 +0300

    Added drag and grid

--- app/scripts/directives/drag.js
+++ app/scripts/directives/drag.js
@@ -7,25 +7,31 @@ app.directive('drag', ['$document', function ($document) {
 
 	return {
 		link: function (scope, element) {
-			
+			var startX = 0, startY = 0, x = 0, y = 0;
 			var dragHandle = element.find('.drag-handle');
 
-			var mousemove = function (e) {
-				console.log(e.pageX);
-			};
-
-			var mouseup = function () {
-				
-			};
+			
 			dragHandle.on('mousedown', function (e) {
 				e.preventDefault();
-				console.log('element', element.offset().left);
-				console.log('e.pageX: ', e.pageX);
-				console.log('e.pageY: ', e.pageY);
+				startX = e.pageX - x;
+				startY = e.pageY - y;
 
 				$document.on('mousemove', mousemove);
 				$document.on('mouseup', mouseup);
 			});
+
+			var mousemove = function (e) {
+				x = e.pageX - startX;
+				y = e.pageY - startY;
+				
+				if ( x%10 == 0) { element.css('left', x); }
+				if ( y%10 == 0) { element.css('top', y); }
+			};
+
+			var mouseup = function () {
+				$document.unbind('mousemove', mousemove);
+				$document.unbind('mouseup', mouseup);
+			};
 		}
 	};
 }]);
