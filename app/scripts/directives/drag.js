201bf5a17703536f906712691b7ecd781d00ac37
Sebi Kovacs <sebi.kovacs@gmail.com>
2014-04-16 10:13:40 +0300

    started working on containment

--- app/scripts/directives/drag.js
+++ app/scripts/directives/drag.js
@@ -9,6 +9,16 @@ app.directive('drag', ['$document', function ($document) {
 		link: function (scope, element) {
 			var startX = 0, startY = 0, x = 0, y = 0;
 			var dragHandle = element.find('.drag-handle');
+			
+			var containerWidth = $('.card-content').width();
+			var containerHeight = $('.card-content').height();
+			var containerX = $('.card-content').offset().left;
+			var containerY = $('.card-content').offset().top;
+			var maxX = containerX + containerWidth;
+			var maxY = containerY + containerHeight;
+
+			console.log(maxX);
+			console.log(maxY);
 
 			
 			dragHandle.on('mousedown', function (e) {
@@ -24,8 +34,13 @@ app.directive('drag', ['$document', function ($document) {
 				x = e.pageX - startX;
 				y = e.pageY - startY;
 				
-				if ( x%10 == 0) { element.css('left', x); }
-				if ( y%10 == 0) { element.css('top', y); }
+				if (x + element.width() < maxX) {
+					if ( x%10 == 0) { element.css('left', x); }	
+				}
+				
+				if (y + element.height() < maxY) {
+					if ( y%10 == 0) { element.css('top', y); }
+				}
 			};
 
 			var mouseup = function () {
