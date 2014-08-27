e45794b49d1545565aed74c277419cdd95a298c8
Ionut Colceriu <contact@ghinda.net>
2014-08-27 19:57:00 +0300

    Upgrade bower components. Upgrade angular-meditor to fix firefox issues.

--- app/scripts/website.js
+++ app/scripts/website.js
@@ -4,33 +4,12 @@
 (function() {
   'use strict';
 
-  var $cardPage;
-  var $tour;
-
   var init = function() {
 
-    $cardPage = $('.card-maker');
-    $tour = $('.tutorial-tour');
-
-//     $('.js-guide-btn').click(showGuide);
-
-  };
-
-  var showGuide = function() {
-    // check if card editor page
-    if($cardPage.length) {
-      $tour.foundation('joyride', 'start');
-
-      return false;
-    }
   };
 
   // foundation properties
   $(document).foundation({
-    joyride: {
-      scroll_speed: 500,
-      modal: false
-    },
     reveal: {
       animation_speed: 80
     }
