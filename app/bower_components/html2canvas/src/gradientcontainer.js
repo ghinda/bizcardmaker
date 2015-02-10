f3de7a64be9f2a04c50f66c4544f7239fb6c6766
Ionut Colceriu <contact@ghinda.net>
2015-02-11 00:35:47 +0200

    Remove winter themes. Add two new picture themes: space, city. Upgrade html2canvas to 0.5.x beta.

---
+++ app/bower_components/html2canvas/src/gradientcontainer.js
@@ -0,0 +1,19 @@
+var Promise = require('./promise');
+
+function GradientContainer(imageData) {
+    this.src = imageData.value;
+    this.colorStops = [];
+    this.type = null;
+    this.x0 = 0.5;
+    this.y0 = 0.5;
+    this.x1 = 0.5;
+    this.y1 = 0.5;
+    this.promise = Promise.resolve(true);
+}
+
+GradientContainer.prototype.TYPES = {
+    LINEAR: 1,
+    RADIAL: 2
+};
+
+module.exports = GradientContainer;
---
+++ app/bower_components/html2canvas/src/imagecontainer.js
@@ -0,0 +1,21 @@
+var Promise = require('./promise');
+
+function ImageContainer(src, cors) {
+    this.src = src;
+    this.image = new Image();
+    var self = this;
+    this.tainted = null;
+    this.promise = new Promise(function(resolve, reject) {
+        self.image.onload = resolve;
+        self.image.onerror = reject;
+        if (cors) {
+            self.image.crossOrigin = "anonymous";
+        }
+        self.image.src = src;
+        if (self.image.complete === true) {
+            resolve(self.image);
+        }
+    });
+}
+
+module.exports = ImageContainer;
