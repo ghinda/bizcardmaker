642c9fd0108c9259d0cc8fe464d7b4c7f078ef93
Ionut Colceriu <contact@ghinda.net>
2015-12-09 11:08:28 +0200

    Change affiliate store format, and add 4 days expiry time.

--- app/scripts/affiliates.js
+++ app/scripts/affiliates.js
@@ -6,8 +6,14 @@
 
   var $modal = document.querySelector('#modal-affiliate');
   var $hideCheckbox = $modal.querySelector('.js-checkbox-affiliate-hide');
-  var affiliateStoreKey = 'bizcardmaker-affiliate-hide-modal';
+  var affiliateStoreKey = 'bizcardmaker-affiliate';
   var affiliateStore = null;
+  var affiliateStoreDefault = JSON.stringify({
+    modalHide: false,
+    modalTime: Date.now()
+  });
+  // 4 days
+  var expireTime = 1000 * 60 * 60 * 24 * 4;
   var i;
 
   var showAffiliateModal = function (e) {
@@ -19,15 +25,23 @@
     // get the localstorage value only on the first click.
     // for the e2e tests.
     if(affiliateStore === null) {
-      affiliateStore = window.localStorage.getItem(affiliateStoreKey);
+      affiliateStore = window.localStorage.getItem(affiliateStoreKey) || affiliateStoreDefault;
 
-      // set checkbox value
-      if (affiliateStore === 'true') {
-        $hideCheckbox.checked = true;
+      affiliateStore = JSON.parse(affiliateStore);
+
+      // migration from initial version where value was not an object
+      if (typeof affiliateStore !== 'object') {
+        affiliateStore = JSON.parse(affiliateStoreDefault);
       }
     }
 
-    if(affiliateStore !== 'true') {
+    // if the store expired, show it again
+    if(affiliateStore.modalHide === true && (Date.now() - affiliateStore.modalTime > expireTime)) {
+      affiliateStore.modalHide = false;
+      $hideCheckbox.checked = false;
+    }
+
+    if(affiliateStore.modalHide !== true) {
       // open the reveal modal
       $($modal).foundation('reveal', 'open');
     }
@@ -44,17 +58,16 @@
 
   var changeAffiliateHideStore = function (e) {
     if (e.target.checked) {
-      affiliateStore = 'true';
+      affiliateStore.modalHide = true;
+      affiliateStore.modalTime = Date.now();
     } else {
-      affiliateStore = 'false';
+      affiliateStore.modalHide = false;
     }
 
-    window.localStorage.setItem(affiliateStoreKey, affiliateStore);
+    window.localStorage.setItem(affiliateStoreKey, JSON.stringify(affiliateStore));
   };
 
   $modal.addEventListener('click', closeModal);
   document.body.addEventListener('click', showAffiliateModal);
-
   $hideCheckbox.addEventListener('change', changeAffiliateHideStore);
-
 })();
