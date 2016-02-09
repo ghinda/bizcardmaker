9ffc0bad1eeca5824d6893d6b06e8d7af5e8a6d7
Ionut Colceriu <contact@ghinda.net>
2016-02-09 11:15:41 +0200

    Pictures instead of icons, in the affiliate modal.

--- app/scripts/util.js
+++ app/scripts/util.js
@@ -2,31 +2,33 @@
  */
 
 (function () {
+  'use strict';
+
   function closest ($elem, selector) {
     // traverse the dom up and find the closest parent
     // that matches the selector.
     // include the current element.
-    var $matches
+    var $matches;
 
     // loop through parents
     while ($elem && $elem !== document) {
       if ($elem.parentNode) {
         // find all siblings that match the selector
-        $matches = $elem.parentNode.querySelectorAll(selector)
+        $matches = $elem.parentNode.querySelectorAll(selector);
         // check if our element is matched (poor-man's Element.matches())
         if ([].indexOf.call($matches, $elem) !== -1) {
-          return $elem
+          return $elem;
         }
 
         // go up the tree
-        $elem = $elem.parentNode
+        $elem = $elem.parentNode;
       } else {
-        return null
+        return null;
       }
     }
 
-    return null
-  };
+    return null;
+  }
 
   window.util = {
     closest: closest
