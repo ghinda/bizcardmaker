89717854256d1d173dfc5ec8017b1676308ff0e1
Ionut Colceriu <contact@ghinda.net>
2016-02-15 17:43:01 +0200

    Fix jshint errors.

--- app/scripts/modal.js
+++ app/scripts/modal.js
@@ -1,12 +1,16 @@
 /* foundation modal tweaks
  */
 
-// add a class on the body, when the modal is opened
-// so we can set position: fixed on the background.
-$(document).on('opened', '[data-reveal]', function () {
-  $(document.body).addClass('modal-opened');
-});
+(function() {
+  'use strict';
 
-$(document).on('closed', '[data-reveal]', function () {
-  $(document.body).removeClass('modal-opened');
-});
+  // add a class on the body, when the modal is opened
+  // so we can set position: fixed on the background.
+  $(document).on('opened', '[data-reveal]', function () {
+    $(document.body).addClass('modal-opened');
+  });
+
+  $(document).on('closed', '[data-reveal]', function () {
+    $(document.body).removeClass('modal-opened');
+  });
+})();
