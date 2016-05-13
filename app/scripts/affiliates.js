addd7572655b6fc80686a735a5350b66a59ccf00
Ionut Colceriu <contact@ghinda.net>
2016-05-13 12:46:34 +0300

    New affiliate modal, with a single random offer.

--- app/scripts/affiliates.js
+++ app/scripts/affiliates.js
@@ -4,23 +4,91 @@
 (function() {
   'use strict';
 
-  var $modal = document.querySelector('#modal-affiliate');
+  var $modal = document.querySelector('.modal-affiliate');
 
   // if we're on a page without the modal
   if (!$modal) {
     return;
   }
 
-  var $hideCheckbox = $modal.querySelector('.js-checkbox-affiliate-hide');
+  var util = window.util;
+  var banners = [
+    {
+      name: 'weebly-site',
+      href: 'http://shareasale.com/r.cfm?b=838308&u=1215865&m=37723&urllink=&afftrack=',
+      src: '/images/affiliate/banner-weebly.gif'
+    },
+    {
+      name: 'weebly-eshop',
+      href: 'http://shareasale.com/r.cfm?b=547761&u=1215865&m=37723&urllink=&afftrack=',
+      src: '/images/affiliate/banner-weebly-eshop.gif'
+    },
+    {
+      name: 'tinyprints-thankyou',
+      href: 'http://shareasale.com/r.cfm?b=524136&u=1215865&m=12808&urllink=&afftrack=',
+      src: '/images/affiliate/banner-tinyprints-thankyou.jpg'
+    },
+    {
+      name: 'tinyprints-birthday',
+      href: 'http://shareasale.com/r.cfm?b=192703&u=1215865&m=12808&urllink=&afftrack=',
+      src: '/images/affiliate/banner-tinyprints-birthday.gif'
+    },
+    {
+      name: 'logaster',
+      href: 'https://www.logaster.com/ref/bizcardmaker/f6a48978.html',
+      src: '/images/affiliate/banner-logaster.png'
+    },
+    {
+      name: 'logaster-custom',
+      href: 'https://www.logaster.com/ref/bizcardmaker/f6a48978.html',
+      src: '/images/affiliate/banner-logaster-custom.png'
+    }
+  ];
+
+  var activeBanner = null;
+  var $modalStatus = document.querySelector('.modal-affiliate-status');
+  var $link = document.querySelector('.modal-affiliate-banner');
+  var $img = $link.querySelector('img');
+
   var affiliateStoreKey = 'bizcardmaker-affiliate';
   var affiliateStore = null;
   var affiliateStoreDefault = JSON.stringify({
     modalHide: false,
     modalTime: Date.now()
   });
-  // 4 days
-  var expireTime = 1000 * 60 * 60 * 24 * 4;
-  var i;
+  var expireTime = 1000;
+
+  var loadingClass = 'modal-affiliate-status-loading';
+  var doneClass = 'modal-affiliate-status-done';
+  var loadingTime = 3200;
+
+  var startLoading = function () {
+    util.removeClass($modalStatus, doneClass);
+    util.addClass($modalStatus, loadingClass);
+
+    setTimeout(function () {
+      util.removeClass($modalStatus, loadingClass);
+      util.addClass($modalStatus, doneClass);
+    }, loadingTime);
+  };
+
+  var updateAffiliateHideStore = function (e) {
+    affiliateStore.modalHide = true;
+    affiliateStore.modalTime = Date.now();
+    window.localStorage.setItem(affiliateStoreKey, JSON.stringify(affiliateStore));
+  };
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function setRandomBanner () {
+    var rand = getRandomInt(0, banners.length);
+    activeBanner = banners[rand];
+
+    $link.href = activeBanner.href;
+    $img.src = activeBanner.src;
+  }
 
   var showAffiliateModal = function (e) {
     if (!window.util.closest(e.target, '.js-affiliate-modal')) {
@@ -43,12 +111,20 @@
     // if the store expired, show it again
     if (affiliateStore.modalHide === true && (Date.now() - affiliateStore.modalTime > expireTime)) {
       affiliateStore.modalHide = false;
-      $hideCheckbox.checked = false;
     }
 
     if (affiliateStore.modalHide !== true) {
+      // set a random banner
+      setRandomBanner();
+
       // open the reveal modal
       $($modal).foundation('open');
+
+      // show loading status
+      startLoading();
+
+      // update shown time
+      updateAffiliateHideStore();
 
       // track affiliate modal views
       ga('send', 'event', 'Affiliate', 'View affiliate modal');
@@ -64,18 +140,18 @@
     e.preventDefault();
   };
 
-  var changeAffiliateHideStore = function (e) {
-    if (e.target.checked) {
-      affiliateStore.modalHide = true;
-      affiliateStore.modalTime = Date.now();
-    } else {
-      affiliateStore.modalHide = false;
-    }
-
-    window.localStorage.setItem(affiliateStoreKey, JSON.stringify(affiliateStore));
-  };
+  function trackBannerClick () {
+    // analytics track banner clicks
+    ga('send', 'event', 'Affiliate', 'Click banner ' + activeBanner.name);
+  }
 
   $modal.addEventListener('click', closeModal);
   document.body.addEventListener('click', showAffiliateModal);
-  $hideCheckbox.addEventListener('change', changeAffiliateHideStore);
+  $link.addEventListener('click', trackBannerClick);
+
+//   setTimeout(function () {
+//     showAffiliateModal({
+//       target: document.querySelector('.js-affiliate-modal')
+//     })
+//   }, 500)
 })();
