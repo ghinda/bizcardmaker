bfe27fa20a992dedf597491e42838a3aaebb5351
Ionut Colceriu <contact@ghinda.net>
2025-06-19 17:42:28 +0300

    Built bizcardmaker from commit 6dfbca1 on branch master

--- scripts/website.js
+++ scripts/website.js
@@ -1,2 +1,2 @@
-"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/service-worker.js").then(function(e){},function(e){})}),$(document).foundation();
+"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register("/service-worker.js").then(function(e){},function(e){})});
 //# sourceMappingURL=website.js.map
